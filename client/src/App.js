75920513411ca03ad61461bf15eaa3b0d488d56a
karandeep-3154 <ksingh310504@gmail.com>
2023-11-29 17:46:16 +0530

    Admin and User Dashboard Created

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,16 @@ import Policy from "./Pages/Policy";
 import PageNotFound from "./Pages/PageNotFound";
 import Register from "./Pages/Auth/Register";
 import Login from "./Pages/Auth/Login";
-import Dashboard from "./Components/User/Dashboard";
+import Dashboard from './Pages/User/Dashboard.js'
 import UserRoute from "./Routes/UserRoute.js";
 import ForgotPassword from "./Pages/Auth/ForgotPassword";
 import AdminDashboard from './Pages/Admin/AdminDashboard.js';
 import AdminRoute from './Routes/AdminRoute.js';
+import CreateCategory from "./Pages/Admin/CreateCategory.js";
+import CreateProduct from "./Pages/Admin/CreateProduct.js";
+import Users from "./Pages/Admin/Users.js";
+import Orders from "./Pages/User/Orders.js";
+import Profile from "./Pages/User/Profile.js";
 
 function App() {
   return (
@@ -20,9 +25,14 @@ function App() {
 
         <Route path="/dashboard" element={<UserRoute />}>
           <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
         </Route>
-        <Route path="/dashboard" element = {<AdminRoute/>}>
-          <Route path = 'admin' element = {<AdminDashboard/>}/>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/users" element={<Users />} />
         </Route>
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/register" element={<Register />} />
