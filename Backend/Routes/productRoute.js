c89e922f6753a8830182c3d736f7ea5c9228032f
karandeep-3154 <ksingh310504@gmail.com>
2023-12-07 19:14:51 +0530

    Updated

--- Backend/Routes/productRoute.js
+++ Backend/Routes/productRoute.js
@@ -4,6 +4,9 @@ import {
   deleteProductController,
   getProductController,
   getSingleProductController,
+  productCountController,
+  productFiltersController,
+  productListController,
   productPhotoController,
   updateProductController,
 } from "../Controller/productController.js";
@@ -41,4 +44,12 @@ router.get("/product-photo/:pid", productPhotoController);
 //delete rproduct
 router.delete("/delete-product/:pid", deleteProductController);
 
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
 export default router;
--- client/src/Pages/Admin/CreateProduct.js
+++ client/src/Pages/Admin/CreateProduct.js
@@ -56,6 +56,7 @@ const CreateProduct = () => {
         toast.error(data?.message);
       } else {
         toast.success("Product Created Successfully");
+     
         navigate("/dashboard/admin/products");
       }
     } catch (error) {
